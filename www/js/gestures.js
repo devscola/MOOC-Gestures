046c4a406901838b4a4e4dfe1ca6a29e543cd3e8
xaviuzz <xaviuzz@gmail.com>
2016-10-05 23:33:36 +0200

    gestos reconocidos

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,11 +1,32 @@
 var app={
   inicio: function(){
-    
+    this.iniciaBotones();
+    this.iniciaFastClick();
+    this.iniciaHammer();
+  },
+
+  iniciaFastClick: function () {
+    FastClick.attach(document.body);
+  },
+  
+  iniciaBotones: function(){
     var botonClaro = document.querySelector('#claro');
     var botonOscuro = document.querySelector('#oscuro');
     
     botonClaro.addEventListener('click',this.ponloClaro,false);
     botonOscuro.addEventListener('click',app.ponloOscuro,false);
+  },
+
+  iniciaHammer: function() {
+    var zona = document.getElementById('zona-gestos');
+    var hammertime = new Hammer(zona);
+    
+    hammertime.get('pinch').set({ enable: true });
+    hammertime.get('rotate').set({ enable: true });
+    
+    hammertime.on('tap doubletap pan swipe press pinch rotate', function(ev) {
+      document.querySelector('#info').innerHTML= ev.type+'!';
+    });
   },
 
   ponloClaro: function(){
@@ -20,7 +41,6 @@ var app={
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
         app.inicio();
     }, false);
 }
